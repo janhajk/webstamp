d84e8082d9fecaf8fe8eadbcfea0c250b6600395
Jan <janhajk@gmail.com>
2018-11-02 13:52:32 +0000

    add

--- popup.js
+++ popup.js
@@ -1,23 +1,9 @@
-angular.module("Demo", []).controller("DemoCtrl", function($scope, $http) {
-  function openInNewTab() {
-    var uri = 'http://martinfowler.com/ieeeSoftware/whenType.pdf';
-    var link = angular.element('<a href="' + uri + '" target="_blank"></a>');
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
 
-    angular.element(document.body).append(link);
 
-    link[0].click();
-    link.remove();
-  };
-  
-  $("#test").click(function(){
-    openInNewTab();
+$("#test").click(function() {
+     openInNewTab('https://login.swissid.ch/idp/XUI/?realm=%2Fsesam&locale=de&authIndexType=service&authIndexValue=Sesam-Push-Auth&goto=https%3A%2F%2Flogin.swissid.ch%2Fidp%2Foauth2%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3Dklp-client%26redirect_uri%3Dhttps%253A%252F%252Faccount.post.ch%252Fredirect%252Fid-confirmation%26scope%3Dopenid%2520email%2520phone%2520profile%26state%3DwANR5Hydktmag494PA_HzbTd0X2zo1GWrNUjTj1F59w%26nonce%3DIMPx9eWEWrSkWwkTr9RUx8_Ou19ymU4TLdVkPXCEH-g%26ui_locales%3Dde%26acr_values%3Dloa-1%26prompt%3D%26acr%3Dloa-1%26acr_sig%3DRU04zf5fCUn6xUQh8lTnDJxp5dtuXtJFcpOis9udsRk#login/');
   });
-  
-  $("#test").click();
-
-  $scope.works = openInNewTab;
-
-  $scope.doesntWork = function() {
-    $http.get('https://api.github.com/users/angular').then(openInNewTab);
-  };
-});
